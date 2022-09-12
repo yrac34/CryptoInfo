c28a5e07f1d03ea7e30d5aab40383ab7193b869a
yrac34 <amyrimchun@gmail.com>
2022-09-11 22:31:47 -0700

    HomeView Header w/ animation

--- CryptoInfo/ContentView.swift
+++ CryptoInfo/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      ZStack {
+        Color.theme.background
+          .ignoresSafeArea()
+        
+        VStack(spacing: 40) {
+          
+          Text("Accent Color")
+            .foregroundColor(Color.theme.accent)
+          
+          Text("Secondary Text Color")
+            .foregroundColor(Color.theme.secondaryText)
+          
+          Text("Red")
+            .foregroundColor(Color.theme.red)
+          
+          Text("Green")
+            .foregroundColor(Color.theme.green)
+        }
+        .font(.headline)
+      }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+      ContentView()
+        .preferredColorScheme(.dark)
     }
 }
--- CryptoInfo/CryptoInfoApp.swift
+++ CryptoInfo/CryptoInfoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct CryptoInfoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          NavigationView{
+            HomeView()
+              .navigationBarHidden(true)
+          }
         }
     }
 }
